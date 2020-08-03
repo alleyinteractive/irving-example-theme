00640d30582dcf6d107a17ea551a688da7e982f1
Owen Stowe <ostowe@gmail.com>
2020-08-03 11:04:47 -0400

    Add coral component

--- client/irving/componentMap.js
+++ client/irving/componentMap.js
@@ -1,5 +1,6 @@
 import ComponentMap from '@irvingjs/styled-components';
 import AdminBar from '@irvingjs/wp-admin-bar';
+import CoralTalkEmbed from 'components/coral';
 import { FiSearch } from 'react-icons/fi';
 import userThemes from './themes';
 
@@ -7,4 +8,5 @@ export default {
   ...ComponentMap(userThemes),
   'irving/admin-bar': AdminBar,
   'irving-icon/search': FiSearch,
+  'irving/coral': CoralTalkEmbed,
 };
